87e0508d157334c70d3722ea01eef60d3feb3d22
Jeff Bryner <jeff@jeffbryner.com>
2025-03-14 17:50:39 -0700

    default to gemini 2

--- illuminAIte.py
+++ illuminAIte.py
@@ -25,9 +25,15 @@ def main():
     )
     parser.add_argument(
         "--model-name",
-        help="Specify the model name/id (e.g. gpt-4, gemini-1.5-pro, llama2)",
-        default="gemini-1.5-flash",
+        help="Specify the model name/id (e.g. gpt-4, gemini-1.5-flash, llama2)",
+        default="gemini-2.0-flash",
     )
+    parser.add_argument(
+        "--location",
+        help="Specify the GCP location (e.g. us-central1)",
+        default="us-central1",
+    )
+
     # since this gets run by the shiny app, we need to pass the args as env vars
     config = parser.parse_args()
     logger.debug(f"Starting illuminAIte with config: {config}")
