b09b949d261329cc8e905e3f7fac3dab6d344e45
Jeff Bryner <jeff@jeffbryner.com>
2025-03-14 17:51:05 -0700

    account for gemini via api key

--- app/gemini_models.py
+++ app/gemini_models.py
@@ -1,11 +1,11 @@
 from agno.models.google import Gemini
 from google.genai import types
 import google.auth
+import os
+import json
 
-credentials, PROJECT_ID = google.auth.default()
-GEMINI_PRO = "gemini-1.5-pro"
-GEMINI_FLASH = "gemini-1.5-flash"
-LOCATION = "us-central1"
+
+config = json.loads(os.environ["_ILLUMINAITE_CONFIG"])
 
 generation_config = types.GenerateContentConfig(
     temperature=0,
@@ -14,11 +14,12 @@ generation_config = types.GenerateContentConfig(
     max_output_tokens=4096,
 )
 
+# TODO for some reason gemini via api key doesn't like category unspecified, enable it if vertex, disable if api key
 safety_settings = [
-    types.SafetySetting(
-        category=types.HarmCategory.HARM_CATEGORY_UNSPECIFIED,
-        threshold=types.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    ),
+    # types.SafetySetting(
+    #     category=types.HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+    #     threshold=types.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    # ),
     types.SafetySetting(
         category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
         threshold=types.HarmBlockThreshold.BLOCK_ONLY_HIGH,
@@ -37,20 +38,31 @@ safety_settings = [
     ),
 ]
 
-model_pro = Gemini(
-    id=GEMINI_PRO,
-    vertexai=True,
-    project_id=PROJECT_ID,
-    location=LOCATION,
-    generation_config=generation_config,
-    safety_settings=safety_settings,
-)
 
-model_flash = Gemini(
-    id=GEMINI_FLASH,
-    vertexai=True,
-    project_id=PROJECT_ID,
-    location=LOCATION,
-    generation_config=generation_config,
-    safety_settings=safety_settings,
-)
+# create the model in vertex or via api key
+def get_gemini_model():
+    model_gemini = None
+    if os.environ.get("GEMINI_API_KEY"):
+
+        model_gemini = Gemini(
+            id=config["model_name"],
+            vertexai=False,
+            api_key=os.environ.get("GEMINI_API_KEY"),
+            generation_config=generation_config,
+            safety_settings=safety_settings,
+        )
+    else:
+        # no api key, we are using vertex
+        credentials, PROJECT_ID = google.auth.default()
+        LOCATION = "us-central1"
+        if config["location"]:
+            LOCATION = config["location"]
+        model_gemini = Gemini(
+            id=config["model_name"],
+            vertexai=True,
+            project_id=PROJECT_ID,
+            location=LOCATION,
+            generation_config=generation_config,
+            safety_settings=safety_settings,
+        )
+    return model_gemini
